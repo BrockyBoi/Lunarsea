c99936ad8446943a5381e429bb8b8f1ffe8c6169
David Donnelly <davidonn@cs.edu>
2017-01-22 11:14:42 -0700

    Adding sink death

---
+++ Assets/Scripts/SinkKill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkKill : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Boat.player.TakeDamage();
+        Boat.player.TakeDamage();
+        Boat.player.TakeDamage();
+    }
+}
