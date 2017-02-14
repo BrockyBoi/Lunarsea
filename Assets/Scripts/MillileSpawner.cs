7ead3ccf87774643a661e413d9066412561badc4
Brock Soicher <brocksoicher@msn.com>
2017-02-13 18:59:27 -0700

    Added blue missile + tracking missiles
    The groundwork is there for a lot more, but here are some basics for later work.

--- Assets/Scripts/MillileSpawner.cs
+++ Assets/Scripts/MillileSpawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MillileSpawner : MonoBehaviour {
 	public GameObject missilePrefab;
+	public GameObject blueMissilePrefab;
+	public GameObject trackerMissile;
 	public GameObject rockPrefab;
 	public GameObject healthPrefab;
 
@@ -15,13 +17,19 @@ public class MillileSpawner : MonoBehaviour {
 
 	float hTimer;
 	float hNextTime;
+
+	IEnumerator rockSpawner;
 	// Use this for initialization
 	void Start () {
 		mNextTime = 3;
 		rNextTime = 3;
 		hNextTime = 5;
 
-		MissileVolley ();
+		//BlueMissileVolleyHigh ();
+		//BlueMissileVolleyLow ();
+		rockSpawner = RockEnum (4);
+		//StartCoroutine (rockSpawner);
+		StartCoroutine (SpawnTrackerMissiles ());
 	}
 	
 	// Update is called once per frame
@@ -61,7 +69,6 @@ public class MillileSpawner : MonoBehaviour {
 	{
 		float minHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, .35f)).y;
 		float maxHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, .9f)).y;
-		//Vector2 offScreen = new Vector2(Camera.main.ViewportToWorldPoint(new Vector3(1,.5f)).x + 5, Random.Range(minHeight,maxHeight));
 		float offScreenX = Camera.main.ViewportToWorldPoint(new Vector3(1,0)).x;
 
 		for (int i = 0; i < 4; i++) {
@@ -69,12 +76,56 @@ public class MillileSpawner : MonoBehaviour {
 		}
 	}
 
+	void BlueMissileVolleyHigh()
+	{
+		float minHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, .45f)).y;
+		float maxHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, .9f)).y;
+		float offScreenX = Camera.main.ViewportToWorldPoint(new Vector3(1,0)).x;
+
+		for (int i = 0; i < 3; i++) {
+			Instantiate (blueMissilePrefab, new Vector3 (offScreenX + 5, minHeight + (i * ((maxHeight - minHeight) / 3.0f))), Quaternion.identity);
+		}
+	}
+
+	void BlueMissileVolleyLow()
+	{
+		float minHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, .1f)).y;
+		float maxHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, .5f)).y;
+		float offScreenX = Camera.main.ViewportToWorldPoint(new Vector3(1,0)).x;
+
+		for (int i = 0; i < 3; i++) {
+			Instantiate (blueMissilePrefab, new Vector3 (offScreenX + 5, minHeight + (i * ((maxHeight - minHeight) / 3.0f))), Quaternion.identity);
+		}
+	}
+
+	IEnumerator SpawnTrackerMissiles()
+	{
+		Vector2 offScreen = Camera.main.ViewportToWorldPoint (new Vector2(Random.Range (0.0f, 1.0f), 1.2f));
+
+		for (int i = 0; i < 4; i++) {
+			Instantiate (trackerMissile, offScreen, Quaternion.identity);
+			yield return new WaitForSeconds (2.5f);
+		}
+	}
+
 	void SpawnRock()
+	{
+		float minRockHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0)).y;
+		float maxRockHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0.15f)).y;
+		float offScreen = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0)).x;
+		Instantiate (rockPrefab, new Vector3 (offScreen+20, Random.Range(minRockHeight,maxRockHeight)), Quaternion.identity);
+	}
+
+	IEnumerator RockEnum(int amount)
 	{
 		float minRockHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0)).y;
 		float maxRockHeight = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0.2f)).y;
 		float offScreen = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0)).x;
-		Instantiate (rockPrefab, new Vector3 (offScreen+20, Random.Range(minRockHeight,maxRockHeight)), Quaternion.identity);
+
+		for (int i = 0; i < amount; i++) {
+			Instantiate (rockPrefab, new Vector3 (offScreen+20, Random.Range(minRockHeight,maxRockHeight)), Quaternion.identity);
+			yield return new WaitForSeconds (1.5f);
+		}
 	}
 
 	void SpawnHealth()
---
+++ Assets/Scripts/TrackingMissile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingMissile : MonoBehaviour {
+	public float speed;
+	public GameObject particles;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (TakeShot ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator TakeShot()
+	{
+		Vector3 unitVector = (Boat.player.transform.position - transform.position).normalized;
+
+		while (true) {
+			transform.position = Vector3.MoveTowards (transform.position, transform.position + unitVector, speed * Time.deltaTime);
+			yield return null;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			HitPlayer (other.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			HitPlayer (other.gameObject);
+		}
+	}
+
+	void HitPlayer(GameObject o)
+	{
+		o.GetComponent<Boat> ().TakeMissileDamage ();
+		AudioController.controller.PlayMissileSound ();
+		Instantiate (particles,transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
